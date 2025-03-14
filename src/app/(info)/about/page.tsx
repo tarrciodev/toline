fa8fdd6c8d92bae93e43957b897cb471bafa1955
tarrciodev <tarcisio.devmaster@gmail.com>
2025-03-14 12:11:34 +0100

    feat: page about created

---
+++ src/app/(info)/about/page.tsx
@@ -0,0 +1,80 @@
+export default function About() {
+    return (
+        <div>
+            <h1 className='text-3xl font-bold  text-blue-600 mb-6'>
+                Sobre a Toline
+            </h1>
+            <p className='text-lg mb-4'>
+                A <strong>Toline</strong> é uma plataforma online que conecta
+                empresas a profissionais qualificados para a realização de
+                projetos de forma rápida e eficiente. Com uma vasta reserva de
+                talentos em diversos domínios, a Toline permite que os
+                utilizadores publiquem os seus projetos e recebam propostas em
+                minutos, garantindo agilidade no processo de contratação.
+            </p>
+
+            <h2 className='text-2xl font-semibold text-blue-600 mt-6'>
+                Nossa Missão
+            </h2>
+            <p className='mb-4'>
+                Fornecer uma plataforma inovadora e acessível que facilita a
+                conexão entre empresas e profissionais, promovendo oportunidades
+                de crescimento, colaboração e desenvolvimento econômico.
+            </p>
+
+            <h2 className='text-2xl font-semibold text-blue-600 mt-6'>
+                Nossos Valores
+            </h2>
+            <ul className='list-disc pl-6 mb-4'>
+                <li>
+                    <strong>Inovação:</strong> Buscamos constantemente aprimorar
+                    nossos serviços para atender às necessidades do mercado.
+                </li>
+                <li>
+                    <strong>Confiança:</strong> Priorizamos a segurança e a
+                    transparência em todas as transações realizadas na
+                    plataforma.
+                </li>
+                <li>
+                    <strong>Eficiência:</strong> Garantimos processos ágeis e
+                    simplificados para facilitar a contratação de profissionais.
+                </li>
+                <li>
+                    <strong>Qualidade:</strong> Trabalhamos para conectar
+                    empresas a profissionais altamente capacitados e
+                    comprometidos com a excelência.
+                </li>
+            </ul>
+
+            <h2 className='text-2xl font-semibold text-blue-600 mt-6'>
+                Nosso Diferencial
+            </h2>
+            <ul className='list-disc pl-6 mb-4'>
+                <li>
+                    <strong>Vasta reserva de talentos:</strong> Aceda a milhares
+                    de profissionais qualificados em diversas áreas.
+                </li>
+                <li>
+                    <strong>Facilidade de uso:</strong> Plataforma intuitiva,
+                    permitindo a publicação de projetos e recebimento de
+                    propostas rapidamente.
+                </li>
+                <li>
+                    <strong>Pagamentos seguros:</strong> O pagamento é realizado
+                    somente após a conclusão do serviço e aprovação do cliente.
+                </li>
+                <li>
+                    <strong>Suporte especializado:</strong> Nossa equipe está
+                    sempre disponível para garantir uma experiência segura e
+                    eficiente.
+                </li>
+            </ul>
+
+            <p className='mt-6 text-lg font-semibold'>
+                A <strong>Toline</strong> compromete-se a fornecer uma
+                experiência eficiente e segura, ligando empresas a profissionais
+                qualificados e contribuindo para o sucesso de seus projetos.
+            </p>
+        </div>
+    );
+}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,42 +1,50 @@
-"use client";
+import { auth } from "@/auth";
+import { DoorOpen } from "lucide-react";
 import Link from "next/link";
 import { Logo } from "./logo";
 import { MenuBar } from "./mobile/menu-bar";
 
-export function Header() {
+export async function Header() {
+    const data = ((await auth()) as { user: { email: string } }) ?? null;
+
     return (
         <header className='bg-white shadow-sm sticky top-0 z-50 overflow-hidden h-16 px-4 sm:px-56'>
             <div className='flex justify-between items-center'>
                 <Logo redirectTo='/' />
-                <nav>
-                    <ul className='flex space-x-6'>
-                        <li>
-                            <a
-                                href='#about'
-                                className='text-gray-700 hover:text-blue-600'
-                            >
-                                About
-                            </a>
-                        </li>
-                        <li>
-                            <Link
-                                href='/blog'
-                                className='text-gray-700 hover:text-blue-600'
-                                prefetch
-                            >
-                                blog
-                            </Link>
-                        </li>
-                    </ul>
+                <nav className='flex gap-6'>
+                    <Link href='/about' prefetch>
+                        About
+                    </Link>
+
+                    <Link
+                        href='/blog'
+                        className='text-gray-700 hover:text-blue-600'
+                        prefetch
+                    >
+                        blog
+                    </Link>
                 </nav>
                 <div className='hidden sm:flex gap-2 items-center'>
-                    <Link href='/login'>Sign in</Link>
-                    <Link
-                        href='/register'
-                        className='bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700'
-                    >
-                        Sign up
-                    </Link>
+                    {data?.user?.email ? (
+                        <Link href='/dash' prefetch>
+                            <span className='flex justify-center items-center gap-2 bg-blue-800 text-blue-50 font-semibold rounded-xl px-4 py-2'>
+                                dashboard <DoorOpen />
+                            </span>
+                        </Link>
+                    ) : (
+                        <>
+                            <Link href='/login' prefetch>
+                                Sign in
+                            </Link>
+                            <Link
+                                href='/register'
+                                prefetch
+                                className='bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700'
+                            >
+                                Sign up
+                            </Link>
+                        </>
+                    )}
                 </div>
                 <MenuBar />
             </div>
