3930ef897628a72ba95236c2e5ed8f448bb2e5db
tarrciodev <tarcisio.devmaster@gmail.com>
2025-03-27 14:11:44 +0100

    feat: app using react-query on server components to prefetch

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { getQueryClient } from "@/lib/get-query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
 import type { ReactNode } from "react";
 
 export function Providers({ children }: { children: ReactNode }) {
-    const queryClient = new QueryClient();
+    const queryClient = getQueryClient();
 
     return (
         <QueryClientProvider client={queryClient}>
